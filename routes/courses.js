67e631478115fed9c2db68ac1316d8e53ac67557
Igor Ilchenko <wog516@gmail.com>
2021-10-01 17:52:31 +0300

    added edit course function

--- routes/courses.js
+++ routes/courses.js
@@ -10,6 +10,21 @@ router.get('/', async (req,res) => {
     courses
   })
 })
+router.get('/:id/edit', async ( req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/')
+  }
+   const course = await Course.getById(req.params.id)
+  res.render('editPage', {
+    title: ` Edit course ${course.title}`,
+    course
+  })
+})
+router.post('/edit', async (req, res) => {
+ await Course.update(req.body)
+  res.redirect('/courses')
+})
+
 router.get('/:id', async (req, res) => {
   const course = await Course.getById(req.params.id)
   res.render('course', {
