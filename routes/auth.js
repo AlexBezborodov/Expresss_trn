80aeacab37a56336b2103c37b6c18d4e14c3ce62
AlexBezborodov <alexbezborodov23@gmail.com>
2021-12-13 16:47:34 +0200

    added registration

--- routes/auth.js
+++ routes/auth.js
@@ -28,4 +28,21 @@ router.post('/login', async (req, res) => {
 
 })
 
+router.post('/register', async (req, res) => {
+  try {
+   const {email, password, confirm, name} = req.body
+   const candidate = await User.findOne({email})
+    if (candidate) {
+      res.redirect('/auth/login#register')
+    }else {
+      const user = new User({email, name, password, cart: {items: []}});
+      debugger;
+      await user.save();
+      res.redirect('/auth/login#login')
+    }
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 module.exports = router
